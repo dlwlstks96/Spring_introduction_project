e6df3f7f2b9e86db68bdec52da35274a0c8c3124
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-08 16:34:38 +0900

    회원 데이터 관련 클래스 구현

---
+++ src/test/java/spring5chap03/chap03/Chap03ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring5chap03.chap03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
