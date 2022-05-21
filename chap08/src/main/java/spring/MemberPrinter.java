d9f3e2f40266cd03701ff909fec745e32a28dbb3
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-21 16:08:25 +0900

    chap03 파일 복사 및 일부분 수정

---
+++ chap08/src/main/java/spring/MemberPrinter.java
@@ -0,0 +1,12 @@
+package spring;
+
+public class MemberPrinter {
+
+    public void print(Member member) {
+        System.out.println("회원 정보: 아이디 = " + member.getId() +
+                ", 이메일 = " + member.getEmail() +
+                ", 이름 = " + member.getName() +
+                ", 등록일 = " + member.getRegisterDateTime());
+    }
+
+}
