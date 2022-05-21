c338d375145601eb011990c0d478af13a79ed249
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-21 15:56:25 +0900

    chap08 프로젝트 생성

---
+++ chap08/src/test/java/spring/chap08/Chap08ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.chap08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
