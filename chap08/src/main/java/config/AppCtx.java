74744bfbb347294dffca173b5dc4746062904b89
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-25 20:35:38 +0900

    전체 기능 연동한 main 코드 구현

--- chap08/src/main/java/config/AppCtx.java
+++ chap08/src/main/java/config/AppCtx.java
@@ -21,12 +21,12 @@ public class AppCtx {
     @Bean(destroyMethod = "close")
     public DataSource dataSource() {
         DataSource ds = new DataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5s?characterEncoding=utf8");
         ds.setUsername("spring5");
         ds.setPassword("spring5");
         ds.setInitialSize(2);
-        ds.setMaxActive(10);
+        ds.setMaxActive(100);
         ds.setTestWhileIdle(true);
         ds.setMinEvictableIdleTimeMillis(60000 * 3);
         ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
