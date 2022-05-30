9907f59298c71ca2f2d543ad899e66fe76adc477
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-30 21:35:52 +0900

    chap09 프로젝트 생성 (War 패키징)

---
+++ chap09/src/main/java/chap09/ServletInitializer.java
@@ -0,0 +1,13 @@
+package chap09;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Chap09Application.class);
+	}
+
+}
---
+++ chap09/src/test/java/chap09/Chap09ApplicationTests.java
@@ -0,0 +1,13 @@
+package chap09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
