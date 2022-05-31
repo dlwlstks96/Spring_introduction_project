038de4b81b1173ca3c9b55b17cb07f3a67584d2e
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-31 21:48:13 +0900

    chap11 프로젝트 생성 및 종속성 추가

---
+++ chap11/src/main/java/chap11/Chap11Application.java
@@ -0,0 +1,13 @@
+package chap11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap11Application.class, args);
+	}
+
+}
