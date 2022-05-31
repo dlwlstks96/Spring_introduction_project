78fdf907b19d2da331128863bd8af0722b27126d
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-31 21:50:51 +0900

    chap08의 일부 코드 복사

---
+++ chap11/src/main/java/spring/MemberDao.java
@@ -0,0 +1,104 @@
+package spring;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    //MemberDao 클래스에 JdbcTemplate 객체 생성
+    public MemberDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    //임의 클래스 이용해서 RowMapper 객체 생성 및 메소드 구현
+    //sql 쿼리문의 ?에 메소드 인자로 넘어온 email 들어감
+    public Member selectByEmail(String email) {
+        List<Member> results = jdbcTemplate.query(
+                "select * from MEMBER where EMAIL = ?",
+                new RowMapper<Member>() { //임의 클래스
+                    @Override
+                    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Member member = new Member(
+                                rs.getString("EMAIL"),
+                                rs.getString("PASSWORD"),
+                                rs.getString("NAME"),
+                                rs.getTimestamp("REGDATE").toLocalDateTime()
+                        );
+                        member.setId(rs.getLong("ID"));
+                        return member;
+                    }
+                },
+                email);
+        return results.isEmpty() ? null : results.get(0);
+    }
+
+    //keyHolder 사용하면 삽입하는 Member 객체의 ID 값을 알 수 있다.
+    //ID 값은 AUTO_INCREMENT 로 자동 증가 칼럼
+    public void insert(final Member member) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pstmt = con.prepareStatement(
+                        "insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE) " +
+                                "values (?, ?, ?, ?)",
+                        new String[]{"ID"});
+                pstmt.setString(1, member.getEmail());
+                pstmt.setString(2, member.getPassword());
+                pstmt.setString(3, member.getName());
+                pstmt.setTimestamp(4,
+                        Timestamp.valueOf(member.getRegisterDateTime()));
+                return pstmt;
+            }
+            }, keyHolder);
+        Number keyValue = keyHolder.getKey();
+        member.setId(keyValue.longValue());
+    }
+
+    //INSERT, UPDATE, DELETE 쿼리는 update() 메소드 이용
+    public void update(Member member) {
+        jdbcTemplate.update(
+                "update MEMBER set NAME = ?, PASSWORD = ? where EMAIL = ?",
+                member.getName(), member.getPassword(), member.getEmail()
+        );
+    }
+
+    public List<Member> selectAll() {
+        List<Member> results = jdbcTemplate.query("select * from MEMBER",
+                new RowMapper<Member>() {
+                    @Override
+                    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        Member member = new Member(
+                                rs.getString("EMAIL"),
+                                rs.getString("PASSWORD"),
+                                rs.getString("NAME"),
+                                rs.getTimestamp("REGDATE").toLocalDateTime()
+                        );
+                        member.setId(rs.getLong("ID"));
+                        return member;
+                    }
+                });
+        return results;
+    }
+
+    //결과가 1행인 경우에만 queryForObject 이용해 간단히 결과 받을 수 있음
+    public int count() {
+        Integer count = jdbcTemplate.queryForObject(
+                "select count(*) from MEMBER", Integer.class
+        );
+        return count;
+    }
+
+}
