b268eb43c7e3a70ed3f2ff07d2c0378895a261bf
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-06-01 21:37:37 +0900

    post 방식 메소드(경로)에 get 방식 요청 시 리다이렉트 처리

--- chap11/src/main/java/controller/RegisterController.java
+++ chap11/src/main/java/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,13 @@ public class RegisterController {
             return "register/step1";
         }
         return "register/step2";
+    }
+
+    //step2는 post 방식만 정상 작동이므로
+    //get 방식에 대해선 리다이렉트 처리
+    @GetMapping("/register/step2")
+    public String handleStep2Get() {
+        return "redirect:/register/step1";
     }
 
 }
