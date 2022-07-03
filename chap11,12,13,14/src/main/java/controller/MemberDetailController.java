5488958c9276005dd9ce4f6093c9ee42a8b5e3ad
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-07-03 15:58:34 +0900

    컨트롤러 익셉션 처리하기
    - @ExceptionHandler 애노테이션 적용한 메소드 구현
    - 해당 익셉션 발생 시 메소드 실행

--- chap11,12,13,14/src/main/java/controller/MemberDetailController.java
+++ chap11,12,13,14/src/main/java/controller/MemberDetailController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import spring.Member;
@@ -27,6 +29,17 @@ public class MemberDetailController {
         }
         model.addAttribute("member", member);
         return "member/memberDetail";
+    }
+
+    //해당 익셉션이 발생하면 에러 응답을 보내는 대신 아래의 메소드 실행
+    @ExceptionHandler(TypeMismatchException.class)
+    public String handleTypeMismatchException() {
+        return "member/invalidId";
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public String handleNotFoundException() {
+        return "member/noMember";
     }
 
 }
